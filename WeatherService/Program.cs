ff9338960a771c39c3191250260653b2458ef435
Edgars Eizvertins <edgars.eizvertins@gmail.com>
2022-08-27 12:09:48 +0300

    Add parameter for location

--- WeatherService/Program.cs
+++ WeatherService/Program.cs
@@ -17,18 +17,34 @@ public class Program
 		CreateHostBuilder(args).Build().Run();
 	}
 
-	public static IHostBuilder CreateHostBuilder(string[] args) {
+	public static IHostBuilder CreateHostBuilder(string[] args)
+	{
 		Console.WriteLine("Hello!");
-		foreach (var arg in args) {
-			Console.WriteLine("Parameter: " + arg);
-		}
-		if (String.IsNullOrEmpty(AppSettings.Settings.WeatherService.AppId)) {
-			AppSettings.Settings.WeatherService.AppId = args.FirstOrDefault();
-		}
+		AddParameters(args);
 		return Host.CreateDefaultBuilder(args)
-			.ConfigureWebHostDefaults(webBuilder => {
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
 				webBuilder.UseStartup<Startup>();
 				webBuilder.UseUrls($"http://{AppSettings.Settings.WeatherService.Host}:{AppSettings.Settings.WeatherService.Port}");
 			});
+	}
+
+	private static void AddParameters(string[] args)
+	{
+		foreach (var arg in args) {
+			Console.WriteLine("Parameter: " + arg);
+		}
+		if (args.Count() > 0) {
+			AppSettings.Settings.WeatherService.AppId = args[0];
+		}
+		if (args.Count() > 1) {
+			AppSettings.Settings.WeatherService.Latitude = args[1];
+		}
+		if (args.Count() > 2) {
+			AppSettings.Settings.WeatherService.Longitude = args[2];
+		}
+		if (args.Count() > 3) {
+			AppSettings.Settings.WeatherService.Units = args[3];
+		}
 	}
 }
